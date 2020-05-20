7ab928474173348e6b1c45f24697c6eaf531d8f8
Hao Wang <hao.wang@beanworks.com>
2020-05-19 22:01:54 -0700

    Added new Project Orthogonal.Persistence.EventStore

---
+++ src/Orthogonal.Persistence.EventStore/RepositoryImpl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Orthogonal.Persistence.EventStore
+{
+    public class RepositoryImpl<T> : Repository<T>
+    {
+        public Task<T> get(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncEnumerable<T> search(Query<T> query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncEnumerable<T> search<TQuery>() where TQuery : Query<T>
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task save(T t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
