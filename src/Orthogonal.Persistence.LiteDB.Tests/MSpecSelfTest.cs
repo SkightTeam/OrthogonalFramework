077d23ab71a1f43756c1e79806ccf5e68aedf1b3
Wang Hao <wanghao@skight.com>
2021-05-07 14:03:21 -0700

    Add Persistence LIteDB project

---
+++ src/Orthogonal.Persistence.LiteDB.Tests/MSpecSelfTest.cs
@@ -0,0 +1,10 @@
+﻿using FluentAssertions;
+using Machine.Specifications;
+
+namespace Orthogonal.Persistence.LiteDB.Tests
+{
+    class MSpecSelfTest
+    {
+        private It should_always_true = () => true.Should().BeTrue();
+    }
+}
